d0efe9274fba8db6f7fd877c90daee890ae3f704
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-04-30 04:52:58 -0400

    step 2 done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('Ayyyy');
+
+function getComputerChoice() {
+    result = Math.random();
+    if (result <= 0.33) {
+        return 'rock';
+    } else if (result > 0.33 && result <= 0.66) {
+        return 'paper';
+    } else {
+        return 'scissors'
+    }
+}
+
+console.log(getComputerChoice());
